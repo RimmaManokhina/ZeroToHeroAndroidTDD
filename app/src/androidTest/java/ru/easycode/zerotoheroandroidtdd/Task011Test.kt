cdde14628a7223c4deb58d04e2a9814e19d88a96
Rimma_M <rimmafon@gmail.com>
2025-05-14 17:39:49 +0300

    task/011-increment

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task011Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task011Test.kt
@@ -14,9 +14,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.runner.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,24 +1,24 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val count = Count.Base(step = 2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        private lateinit var textView : TextView
-        private lateinit var button : Button
-        var step : Int = 2
-        var number : Int = 0
-        var increment : Int = number + step
-
+        val textView = findViewById<TextView>(R.id.countTextView)
+        val button = findViewById<Button>(R.id.incrementButton)
 
         button.setOnClickListener {
-            textView.text == increment.toString()
+            val result = count.increment(textView.text.toString())
+            textView.text = result
         }
 
 
